bf502b9b19cf90b8a3fe1b270191dd870b050b56
Jichao Wu <wujichao@gmail.com>
2015-06-17 14:53:14 +0800

    update demo

--- Example/KRVideoPlayer/FirstViewController.m
+++ Example/KRVideoPlayer/FirstViewController.m
@@ -26,16 +26,24 @@
     [b setTitle:@"remote" forState:UIControlStateNormal];
     [b sizeToFit];
     [b addTarget:self action:@selector(playRemoteVideo:) forControlEvents:UIControlEventTouchUpInside];
-    b.center = CGPointMake(100, 100);
+    b.center = CGPointMake(100, 200);
     [self.view addSubview:b];
     
     UIButton *b1 = [UIButton new];
     b1.backgroundColor = [UIColor redColor];
     [b1 setTitle:@"streaming" forState:UIControlStateNormal];
     [b1 sizeToFit];
-    [b1 addTarget:self action:@selector(playLocalVideo:) forControlEvents:UIControlEventTouchUpInside];
-    b1.center = CGPointMake(100, 200);
+    [b1 addTarget:self action:@selector(playStreamingVideo:) forControlEvents:UIControlEventTouchUpInside];
+    b1.center = CGPointMake(100, 300);
     [self.view addSubview:b1];
+    
+    UIButton *b2 = [UIButton new];
+    b2.backgroundColor = [UIColor redColor];
+    [b2 setTitle:@"local" forState:UIControlStateNormal];
+    [b2 sizeToFit];
+    [b2 addTarget:self action:@selector(playLocalVideo:) forControlEvents:UIControlEventTouchUpInside];
+    b2.center = CGPointMake(100, 400);
+    [self.view addSubview:b2];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -45,15 +53,19 @@
 
 - (IBAction)playLocalVideo:(id)sender
 {
-    //    NSURL *videoURL = [[NSBundle mainBundle] URLForResource:@"150511_JiveBike" withExtension:@"mov"];
-    //  [self playVideoWithURL:videoURL];
-    NSURL *videoURL = [NSURL URLWithString:@"http://219.232.160.141:5080/hls/c64024e7cd451ac19613345704f985fa.m3u8"];
+    NSURL *videoURL = [[NSBundle mainBundle] URLForResource:@"150511_JiveBike" withExtension:@"mov"];
     [self playVideoWithURL:videoURL];
 }
 
 - (IBAction)playRemoteVideo:(id)sender
 {
     NSURL *videoURL = [NSURL URLWithString:@"http://krtv.qiniudn.com/150522nextapp"];
+    [self playVideoWithURL:videoURL];
+}
+
+- (IBAction)playStreamingVideo:(id)sender
+{
+    NSURL *videoURL = [NSURL URLWithString:@"http://219.232.160.141:5080/hls/c64024e7cd451ac19613345704f985fa.m3u8"];
     [self playVideoWithURL:videoURL];
 }
 
